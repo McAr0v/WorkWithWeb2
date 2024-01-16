4696e61bf059dca978041ea12734dc034abd8e33
McAr0v <dvijkz@gmail.com>
2024-01-16 16:48:55 +0600

    Добавьте файлы проекта.

---
+++ WorkWithWeb2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace WorkWithWeb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? exit = null;
+
+            while (exit == null || exit.ToLower() != "exit")
+            {
+                if (args.Length == 0)
+                {
+                    Server.AcceptMsg();
+                    
+                }
+                else
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        new Thread(() => {
+
+                            Client.SendMsg($"Alex {i}");
+
+                        }).Start();
+                    }
+                }
+
+                exit = Convert.ToString(Console.ReadLine());
+
+            }
+            Environment.Exit(0);
+
+        }
+    }
+}
---
+++ WorkWithWeb2/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithWeb2
+{
+    internal class Server
+    {
+        public static void AcceptMsg()
+        {
+            bool isWorked = true;
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient udpClient = new UdpClient(16874);
+            Console.WriteLine("Сервер ожидает сообщение");
+
+            byte[] buffer = udpClient.Receive(ref ep);
+            string data = Encoding.UTF8.GetString(buffer);
+
+            Thread tr = new Thread(() =>
+            {
+                Message msg = Message.FromJson(data);
+                Console.WriteLine(msg.ToString());
+                Message responseMsg = new Message("Server", "Message accept on serv");
+                string responseMsgJs = responseMsg.ToJson();
+                byte[] responseDate = Encoding.UTF8.GetBytes(responseMsgJs);
+                udpClient.Send(responseDate, ep);
+
+            });
+
+            tr.Start();
+
+        }
+    }
+}
